9189b9cf556eb8473c0c0a62e447675dc1e398b9
Matias Fiordelli <>
2025-01-25 18:20:13 -0300

    fix: Adding conditional publicPath for development and production environments in webpack config

--- host/webpack.config.js
+++ host/webpack.config.js
@@ -8,107 +8,110 @@ const deps = require("./package.json").dependencies;
 const printCompilationMessage = require('./compilation.config.js');
 
 module.exports = (_, argv) => ({
-  output: {
-    publicPath: "http://localhost:3000/",
-  },
+	output: {
+		publicPath:
+			argv.mode === "development" 
+				? "http://localhost:3000/"
+				: "https://dev-labs-ten.vercel.app/",
+	},
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
+	resolve: {
+		extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+	},
 
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    },
-    hot: true,
-    port: 3000,
-    historyApiFallback: true,
-    watchFiles: [path.resolve(__dirname, 'src')],
-    onListening: function (devServer) {
-      const port = devServer.server.address().port
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "dist"),
+		},
+		hot: true,
+		port: 3000,
+		historyApiFallback: true,
+		watchFiles: [path.resolve(__dirname, 'src')],
+		onListening: function (devServer) {
+			const port = devServer.server.address().port
 
-      printCompilationMessage('compiling', port)
+			printCompilationMessage('compiling', port)
 
-      devServer.compiler.hooks.done.tap('OutputMessagePlugin', (stats) => {
-        setImmediate(() => {
-          if (stats.hasErrors()) {
-            printCompilationMessage('failure', port)
-          } else {
-            printCompilationMessage('success', port)
-          }
-        })
-      })
-    }
-  },
+			devServer.compiler.hooks.done.tap('OutputMessagePlugin', (stats) => {
+				setImmediate(() => {
+					if (stats.hasErrors()) {
+						printCompilationMessage('failure', port)
+					} else {
+						printCompilationMessage('success', port)
+					}
+				})
+			})
+		}
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
-        },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
+	module: {
+		rules: [
+			{
+				test: /\.m?js/,
+				type: "javascript/auto",
+				resolve: {
+					fullySpecified: false,
+				},
+			},
+			{
+				test: /\.(css|s[ac]ss)$/i,
+				use: ["style-loader", "css-loader", "postcss-loader"],
+			},
+			{
+				test: /\.(ts|tsx|js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+		],
+	},
 
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "host",
-      filename: "remoteEntry.js",
-      remotes: {
-        layout: "layout@http://localhost:3001/remoteEntry.js",
-        content: "content@http://localhost:3002/remoteEntry.js",
-      },
-      exposes: {
-        "./pathRoutes": "./src/utils/helpers/pathRoutes1.ts",
-        "./useVerifyToken": "./src/hooks/useVerifyToken.ts",
-      },
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-        "react-router-dom": {
-          singleton: true,
-        },
-        tailwindcss: {
-          singleton: true,
-        },
-        "framer-motion": {
-          singleton: true,
-        },
-        "redux": {
-          singleton: true,
-        },
-        "react-redux": {
-          singleton: true,
-        },
-        "@reduxjs/toolkit": {
-          singleton: true,
-        }
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-    new Dotenv()
-  ],
+	plugins: [
+		new ModuleFederationPlugin({
+			name: "host",
+			filename: "remoteEntry.js",
+			remotes: {
+				layout: "layout@http://localhost:3001/remoteEntry.js",
+				content: "content@http://localhost:3002/remoteEntry.js",
+			},
+			exposes: {
+				"./pathRoutes": "./src/utils/helpers/pathRoutes1.ts",
+				"./useVerifyToken": "./src/hooks/useVerifyToken.ts",
+			},
+			shared: {
+				...deps,
+				react: {
+					singleton: true,
+					requiredVersion: deps.react,
+				},
+				"react-dom": {
+					singleton: true,
+					requiredVersion: deps["react-dom"],
+				},
+				"react-router-dom": {
+					singleton: true,
+				},
+				tailwindcss: {
+					singleton: true,
+				},
+				"framer-motion": {
+					singleton: true,
+				},
+				"redux": {
+					singleton: true,
+				},
+				"react-redux": {
+					singleton: true,
+				},
+				"@reduxjs/toolkit": {
+					singleton: true,
+				}
+			},
+		}),
+		new HtmlWebPackPlugin({
+			template: "./src/index.html",
+		}),
+		new Dotenv()
+	],
 });
